c6fb4b8d8a34c068ebe0b685905d49f01f5edafa
Vladlen Dumanskyi <vladlen.dumanskyi@gmail.com>
2025-04-13 23:08:51 +0300

    feat: init shad-cn library

--- frontend/dashboard/nuxt.config.ts
+++ frontend/dashboard/nuxt.config.ts
@@ -1,5 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  ssr: false,
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['shadcn-nuxt']
 })
